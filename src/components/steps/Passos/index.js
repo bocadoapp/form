d5ad46db4b739b10119931aba58f53a236567abe
manelgarcia <manelet@gmail.com>
2020-05-06 15:36:10 +0200

    feat: multiple style improvements

--- src/components/steps/Passos/index.js
+++ src/components/steps/Passos/index.js
@@ -1,58 +1,87 @@
-import React, { useCallback, useRef } from 'react'
-import { FieldArray } from 'formik'
+import React, { useCallback, useRef, useEffect } from 'react'
+import { FieldArray, useFormikContext } from 'formik'
 import { Button } from '@bocado/ui'
 
 import withAnimation from '../../../hoc/withAnimation'
+import { useStore } from '../../../hooks/useStore'
 
-function AddStep ({ push }) {
+const createMarkup = __html => ({ __html })
+
+function AddStep ({ passos, push }) {
   const refs = {
     text: useRef(null),
   }
 
   const handleClick = useCallback(e => {
-    const step = {
-      text: refs.text.current.value
-    }
-    push(step)
+    push({ text: refs.text.current.value })
+    refs.text.current.value = ''
   }, [push, refs.text])
 
   return (
-    <div className='flex flex-col w-full'>
+    <div className='flex flex-col w-full bg-gray-100 px-4 py-2 rounded mb-4'>
+      <h3 className='text-3xl'>
+        Pas {passos.length + 1}
+      </h3>
+      <div className="flex justify-between my-4">
+        <Button
+          type='button'
+          className='w-32 items-center'
+          styled='default'
+          onClick={handleClick}
+          size='sm'
+        >
+          <i className="far fa-images mr-3" />
+          Pujar foto
+        </Button>        
+        <Button
+          type='button'
+          className='w-32 items-center'
+          styled='success'
+          onClick={handleClick}
+          size='sm'
+        >
+          <i className="fas fa-plus mr-3" />
+          Afegir
+        </Button>
+      </div>
       <div className='w-full flex'>
         <textarea
           ref={refs.text}
           className='w-full border border-gray-300 rounded p-3 my-3 h-64 text-gray-700'
           placeholder='Explicació del pas a seguir'
         />
-      </div>
-      <Button type='button' className='w-64 m-auto' styled='success' onClick={handleClick}>
-        Afegir
-      </Button>      
+      </div>   
     </div>
   )
 }
 
 const Steps = () => {
+  const { setBtn } = useStore()
+  const { values: { passos } } = useFormikContext()
+
+  useEffect(() => {    
+    return setBtn({ disabled: !passos.length })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [passos.length])
+
   return (
     <div className='w-full'>
       <FieldArray
-        name='steps'
-        render={({ form: { values }, remove, push }) => {
+        name='passos'
+        render={({ form: { values }, remove, push }) => {          
           return (
             <>
-              <div className='flex'>              
-                <Button styled='gradient' className='ml-2' disabled={values.steps.length === 0}>
-                  Fet!
-                </Button>
-              </div>
-              <AddStep push={push} />
-              {values.steps.map(({ text }, i) => {
+              <AddStep passos={values.passos} push={push} />
+              {values.passos.map(({ text }, i) => {
                 return (
-                  <div key={`step-${i}`} className='flex items-center justify-between py-2 my-2 border-b border-gray-200 text-gray-500'>
-                     <span>
-                      <span className='text-gray-700'>{text}</span>
-                     </span>
-                     <i className="far fa-times-circle cursor-pointer" onClick={() => remove(i)} />
+                  <div key={`step-${i}`} className='flex flex-col py-2 my-2 border-b border-gray-200'>
+                    <div className='flex items-center'>
+                      <i className="far fa-times-circle cursor-pointer mr-3" onClick={() => remove(i)} />
+                      <h3 className='text-3xl'>
+                        Pas {i + 1}
+                      </h3>
+                    </div>
+                    <p dangerouslySetInnerHTML={createMarkup(text.replace(/\r?\n/g, '<br />'))} />
                   </div>
                 )
               })}
