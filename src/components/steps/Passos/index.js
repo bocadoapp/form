1d8577b67ed87652887dce3c17c738fc288b325c
manelgarcia <manelet@gmail.com>
2020-05-12 06:42:13 +0200

    fix: linter

--- src/components/steps/Passos/index.js
+++ src/components/steps/Passos/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect } from 'react'
 import { FieldArray, useFormikContext } from 'formik'
 
 import withAnimation from '../../../hoc/withAnimation'
