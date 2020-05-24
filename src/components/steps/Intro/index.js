f0fdced5b40e3ebddc76d1c91bda34e6c0092182
manelgarcia <manelet@gmail.com>
2020-05-24 10:46:14 +0200

    refactor: Intro step

--- src/components/steps/Intro/index.js
+++ src/components/steps/Intro/index.js
@@ -8,6 +8,7 @@ import { Register, Login } from '../../Auth'
 import { useStore } from '../../../hooks/useStore'
 import withAnimation from '../../../hoc/withAnimation'
 import Content from './Content'
+import { getErrorMessage } from '../../../lib/helpers'
 
 // const GET_FILE = gql`
 //   query getFile ($name: String!) {
@@ -18,8 +19,6 @@ import Content from './Content'
 //     }
 //   }
 // `
-
-const LS_KEY = 'bocado_user'
 
 const GET_USER_FROM_TOKEN = gql`
   query getUser ($token: String!) {
@@ -36,11 +35,11 @@ const Intro = () => {
   const [showRegister, toggleShowRegister] = useState(true)
   const [token, setToken] = useState(null)
   const history = useHistory()
-  const { user, setUser } = useStore()
+  const { user, status, setUser, setStatus } = useStore()
   const { locale } = useIntl()
   const { loading, data } = useQuery(GET_USER_FROM_TOKEN, { skip: token === null, variables: { token }})
-  const handleOnlogin = provider => {
-    const url = `${process.env.REACT_APP_API}/auth/${provider}`
+  const handleOnlogin = provider => {     
+    const url = `${process.env.REACT_APP_API}/auth/${provider}?from=${encodeURIComponent(window.location.href)}&tesT=ok`
     window.location.href = url
   }
   const saveAndGoToStep2 = useCallback(user => {
@@ -51,29 +50,30 @@ const Intro = () => {
   }, [history, setUser, locale])
 
   const toggleShowRegisterPerf = useCallback(() => toggleShowRegister(!showRegister), [showRegister])
+  const qs = new URLSearchParams(history.location.search)
+  const tokenQs = qs.get('token')
+  const errCode = qs.get('errCode')
+  const errMessage = qs.get('errMessage')
 
   useEffect(() => {
+    console.log('entro useEffect', loading, data, user, tokenQs, errCode, errMessage)
     if (!user && !loading && data) {
-      localStorage.setItem(LS_KEY, JSON.stringify(data.user))
+      console.log('entro 1');
       saveAndGoToStep2(data.user)
-    }
-  }, [loading, data, saveAndGoToStep2, user])
-
-  useEffect(() => {
-    if (!user) {
-      const token = new URLSearchParams(history.location.search).get('token')
-      if (token) {
-        setToken(token)
-      }
-    }
-  }, [history.location.search, user])
-
-  useEffect(() => {
-    if (user) {
+    } else if (!user && tokenQs) {
+      setToken(tokenQs)
+    } else if (user) {
       saveAndGoToStep2()
+    } else if ((errCode || errMessage) && !status.text) {
+      setStatus({
+        error: true,
+        text: getErrorMessage({ graphQLErrors: [{ message: errMessage, extensions: { exception: { code: errCode, errmsg: errMessage }} }] })
+      })
+    } else if (status.text) {
+      history.replace(history.location.pathname)
     }
-  }, [user, saveAndGoToStep2])
-  
+  }, [loading, data, saveAndGoToStep2, user, tokenQs, errCode, errMessage, setStatus, status.text, history])
+
   return (
     <div>
       <div className='flex flex-col md:flex-row justify-center items-center'>
