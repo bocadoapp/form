05fdae2e4cec0ed1a0d07199b4d1df885bf77384
manelgarcia <manelet@gmail.com>
2020-05-24 10:49:39 +0200

    refactor: login/register

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,50 +1,97 @@
-import React from 'react'
+import React, { useRef, useState, useCallback } from 'react'
 import { useIntl } from 'react-intl'
 import { Button } from '@bocado/ui'
+import { useMutation, gql } from '@apollo/client'
+import cn from 'classnames'
+import { useHistory } from 'react-router-dom'
 
+import { useStore } from '../../hooks/useStore'
+import { getErrorMessage } from '../../lib/helpers'
+import { parseYupErrors, refsValuesToObject, registerSchema } from '../../lib/validate'
 import Wrapper from './Wrapper'
+import Social from './Social'
+
+const CREATE_USER_MUTATION = gql`
+  mutation ($password: String!, $name: String!, $mail: String!) {
+    signup(record:{ password: $password, name: $name, mail: $mail }) {
+      record {
+        name
+        mail
+        username
+        pic
+        points
+      }
+    }
+  }
+`
 
 const Register = ({ loading, handleOnlogin, toggleShowRegister }) => {
-  const { formatMessage: t } = useIntl()
-
+  const history = useHistory()
+  const { setUser, setStatus } = useStore()
+  const [errors, updateErrors] = useState({})
+  const { locale, formatMessage: t } = useIntl()
+  const refs = {
+    name: useRef(null),
+    mail: useRef(null),
+    password: useRef(null),
+    passwordRepeat: useRef(null)
+  }
+  const [createUser, { loading: loadingMutation }] = useMutation(CREATE_USER_MUTATION)
+  const handleOnRegister = useCallback(async e => {    
+    e.preventDefault()
+    const values = refsValuesToObject(refs)
+    
+    try {
+      await registerSchema.validate(values, { abortEarly: false })
+      setStatus({ error: false, text: null })
+      updateErrors({})      
+      createUser({ variables: { name: values.name, mail: values.mail, password: values.password }})
+        .then(
+          response => {
+            setUser(response.data.signup.record)
+            setStatus({ error: false, text: `${values.name} registered!` })
+            history.push(`/${locale}/2`)
+          },
+          err => setStatus({ error: true, text: getErrorMessage(err) })
+        )      
+    } catch (errs) {
+      const [msgs, keys] = parseYupErrors(errs)
+      updateErrors(keys)
+      setStatus({ error: true, text: msgs.join('<br />') })
+    }
+  }, [refs, createUser, setStatus, history, locale, setUser])
+  
   return (
     <Wrapper>
       <div className='flex flex-col justify-center bg-gray-100 p-3 rounded-lg shadow-xl'>
         <div className='mb-3'>
-          <input className='input' type="text" placeholder='Nom i cognoms'/>
+          <input required className={cn('input', errors.name && 'error')} type="text" placeholder='Nom i cognoms' ref={refs.name} />
         </div>
         <div className='mb-3'>
-          <input className='input' type="email" placeholder='Correu electrònic'/>
+          <input required className={cn('input', errors.mail && 'error')} type="email" placeholder='Correu electrònic' ref={refs.mail} />
         </div>
-        <button className='border bg-gray-300 hover:bg-gray-400 text-gray-500'>
+        <div className='mb-3'>
+          <input required className={cn('input', errors.password && 'error')} type="password" placeholder='Contrassenya' ref={refs.password} />
+        </div>
+        <div className='mb-3'>
+          <input required className={cn('input', errors.password && 'error')} type="password" placeholder='Repeteix contrassenya' ref={refs.passwordRepeat} />
+        </div>              
+        <Button
+          onClick={handleOnRegister}
+          loading={loadingMutation}
+          className='border bg-gray-300 hover:bg-gray-400 text-gray-500'
+        >
           {t({ id: 'registre' })}
-        </button>
+        </Button>
         <p className='cursor-pointer m-0 mx-auto mt-3 text-xs text-gray-400' onClick={toggleShowRegister}>
-        {t({ id: 'registre_login' })}
+          {t({ id: 'registre_login' })}
         </p>
       </div>
 
       <p className='text-xs text-gray-400 text-center'>o bé registra't amb</p>
 
       <div className='flex'>
-        <Button
-          loading={loading}
-          onClick={() => handleOnlogin('facebook')}
-          className='items-center text-gray-100 bg-blue-700 hover:bg-blue-800 rounded-r'
-          size='sm'
-        >
-          <i className="fab fa-facebook mr-3" />
-          {t({ id: 'registre_fb' })}
-        </Button>
-        <Button
-          loading={loading}
-          onClick={() => handleOnlogin('google')}
-          className='items-center text-gray-100 bg-red-600 hover:bg-red-700 rounded-l'
-          size='sm'
-        >
-          <i className="fab fa-google mr-3" />
-          {t({ id: 'registre_google' })}
-        </Button>        
+        <Social loading={loading} handleOnlogin={handleOnlogin} />      
       </div>
     </Wrapper>
   )
