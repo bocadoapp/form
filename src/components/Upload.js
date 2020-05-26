8a5d8b564affd650da188f4729421d288ff2ce0b
manelgarcia <manelet@gmail.com>
2020-05-26 17:57:04 +0200

    feat: Upload as render prop component with logic only

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,55 +1,45 @@
-import React, { useCallback } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import { useMutation, gql } from '@apollo/client'
 import { useDropzone } from 'react-dropzone'
-import { useIntl } from 'react-intl'
 
 const UPLOAD_FILE = gql`
-  mutation Upload($file: Upload) {
-    upload(file: $file) {
+  mutation Upload($files: [Upload!]!) {
+    upload(files: $files) {
       _id
+      url
+      path
     }
   }
 `
 
-const Upload = ({ images, setImages }) => {
-  const { formatMessage: t } = useIntl()
-  const [upload, { data }] = useMutation(UPLOAD_FILE)
-  const onDrop = useCallback(acceptedFiles => {
-    setImages([
-      ...images,
-      ...acceptedFiles.map(file =>({ ...file, preview: URL.createObjectURL(file) }))
-    ])
-    upload({ variables: { file: acceptedFiles[0] } })    
-  }, [images, setImages, upload])
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: 'image/*' })
+const Upload = ({ children, beforeUpload, afterUpload }) => {
+  const [previews, setPreviews] = useState([]) 
+  const [upload, { loading }] = useMutation(UPLOAD_FILE)
 
-  return (
-    <>
-      {!!images && !!images.length && (
-        <div className="flex my-3 w-full overflow-x-hidden max-w-full">
-          <div className="flex overflow-x-scroll">
-            {images.map(file => (
-              <div key={file.name || file.path} className='pic mr-3 flex-shrink-0'>
-                <div
-                  className='pic-inner w-full'
-                  style={{
-                    backgroundImage: `url(${file.preview})`,
-                    backgroundSize: 'cover'
-                  }} />
-              </div>
-            ))}
-          </div>
-        </div>   
-      )}
-      <div {...getRootProps()} className='flex flex-col w-full items-center border-4 border-gray-200 border-dashed rounded overflow-hidden'>
-        <input {...getInputProps()} />
-        <p className='text-center'>
-          <i className="far fa-images mr-3 text-3xl" /><br />
-          {t({ id: 'upload_text' })}
-        </p>      
-      </div>         
-    </>
-  )
+  const onDrop = useCallback(acceptedFiles => {
+    if (beforeUpload && typeof beforeUpload === 'function') {
+      beforeUpload({ files: acceptedFiles, previews, setPreviews })
+    }
+
+    upload({ variables: { files: acceptedFiles } })
+      .then(response => {
+        if (afterUpload && typeof afterUpload === 'function') {
+          afterUpload(response)
+        }
+      })
+  }, [previews, setPreviews, upload, beforeUpload, afterUpload])
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: 'image/*' })  
+
+  useEffect(() => () => previews.forEach(file => URL.revokeObjectURL(file.preview)), [previews])  
+
+  return children({
+    previews,
+    setPreviews,
+    getRootProps,
+    getInputProps,
+    loading
+  })
 }
 
 export default Upload
